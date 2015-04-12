2bda996d9296e4e0e58717de6304fc80924296ef
ChristinaHammer <hammec@rpi.edu>
2015-04-11 23:16:19 -0400

    Making App with Silverlight now

---
+++ App1/App1/FBSilver/FBSilver/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using FBSilver.Resources;
+
+namespace FBSilver
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ App1/App1/PhoneApp1/PhoneApp1/FBLogin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+
+namespace FacebookControls_WP8
+{
+    public partial class FBLogin : PhoneApplicationPage
+    {
+        public FBLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSessionStateChanged(object sender, Facebook.Client.Controls.SessionStateChangedEventArgs e)
+        {
+            this.ContentPanel.Visibility = (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Opened) ?
+                            Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
